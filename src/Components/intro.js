fae01da2618cab9b680ba5d4d26899d420877adc
DRRobertson9673 <drrobertson9673@gmail.com>
2023-03-31 18:33:33 +0100

    added skills icons

--- src/Components/intro.js
+++ src/Components/intro.js
@@ -15,7 +15,7 @@ function Intro() {
             if ((scrollTop + (vh)) > Offset) {
                 $('h1').css({
                     opacity: 1,
-                    Animation: `introLeftAnim 1s ease-in-out 0s 1 normal both`
+                    Animation: `introTextAnim 1s ease-in-out 0s 1 normal both`
                 })
             }
         }
@@ -37,7 +37,7 @@ function Intro() {
             } else if ((scrollTop + (vh)) > Offset) {
                 $('#socialLinks').css({
                     opacity: 1,
-                    Animation: `socialLinksAnim .5s ease 1s 1 normal forwards`
+                    Animation: `socialLinksAnim .5s ease 0s 1 normal forwards`
                 })
                 index++
             }
@@ -46,13 +46,13 @@ function Intro() {
 
     return (
         <div id="introArea">
-            <div className="introLeft">
+            <div className="introText">
                 <h1>Hello. I'm Dave</h1>
-                <h2 id="introText">I'm a <span>graphic designer</span> and <span>frontend developer</span> living in Devon and I'm passionate about creating things that are beautiful both inside and out.</h2>
+                <h2>I'm a <span>graphic designer</span> and <span>frontend developer</span> living in Devon and I'm passionate about creating things that are beautiful both inside and out.</h2>
                 <div id="socialLinksTrigger"></div>
-                <p id="introText">As a graphic designer I have 13+ years of experience working with Adobe Creative suite - primarily <span>Photoshop</span>, <span>Illustrator</span> and <span>Indesign</span> all of which I use on a daily basis. As for web development I have a certification in frontend development using <span>Node.js</span>, <span>React</span>, <span>Bootstrap</span>, <span>Javascipt</span>, <span>CSS</span> and <span>HTML</span>.<br /><br />I look at any project or opportunity to work in a design environment as a chance to learn and develop my skillset.</p>
+                <p>As a graphic designer I have 13+ years of experience working with Adobe Creative suite - primarily <span>Photoshop</span>, <span>Illustrator</span> and <span>Indesign</span> all of which I use on a daily basis. As for web development I have a certification in frontend development using <span>Node.js</span>, <span>React</span>, <span>Bootstrap</span>, <span>Javascipt</span>, <span>CSS</span> and <span>HTML</span>.<br /><br />I look at any project or opportunity to work in a design environment as a chance to learn and develop my skillset.</p>
             </div>
-            <div className="introRight">
+            <div className="introImage">
                 <Monitor />
             </div>
         </div>
--- src/Components/skills.js
+++ src/Components/skills.js
@@ -1,4 +1,16 @@
-import "../Style/intro.css"
+import "../Style/skills.css"
+import photoshop from '../Images/icons/photoshop-green.svg';
+import illustrator from '../Images/icons/illustrator-green.svg';
+import indesign from '../Images/icons/indesign-green.svg';
+import HTML from '../Images/icons/HTML-green.svg';
+import CSS from '../Images/icons/CSS-green.svg';
+import js from '../Images/icons/js-green.svg';
+import react from '../Images/icons/react-green.svg';
+import bootstrap from '../Images/icons/bootstrap-green.svg';
+import jquery from '../Images/icons/jquery-green.svg';
+import node from '../Images/icons/node-green.svg';
+import vsc from '../Images/icons/vsc-green.svg';
+import git from '../Images/icons/git-green.svg';
 import React, { useEffect } from 'react';
 import $ from 'jquery';
 
@@ -8,48 +20,104 @@ function Skills() {
         window.addEventListener("scroll", onScroll);
         function onScroll() {
             const scrollTop = $(window).scrollTop();
-            const Offset = document.getElementById('introArea').offsetTop;
+            const Offset = document.getElementById('skillsRow').offsetTop;
             const vh = (window.innerHeight);
 
             if ((scrollTop + (vh)) > Offset) {
-                $('h1').css({
-                    opacity: 1,
-                    Animation: `introLeftAnim 1s ease-in-out 0s 1 normal both`
+                $('#photoshop').css({
+                    animation: `skillAnim 1s ease 0s 1 normal forwards`
+                })
+                $('#illustrator').css({
+                    animation: `skillAnim 1s ease .1s 1 normal forwards`
+                })
+                $('#indesign').css({
+                    animation: `skillAnim 1s ease .2s 1 normal forwards`
+                })
+                $('#HTML').css({
+                    animation: `skillAnim 1s ease .3s 1 normal forwards`
+                })
+                $('#CSS').css({
+                    animation: `skillAnim 1s ease .4s 1 normal forwards`
+                })
+                $('#js').css({
+                    animation: `skillAnim 1s ease .5s 1 normal forwards`
+                })
+                $('#jquery').css({
+                    animation: `skillAnim 1s ease .6s 1 normal forwards`
+                })
+                $('#react').css({
+                    animation: `skillAnim 1s ease .7s 1 normal forwards`
+                })
+                $('#bootstrap').css({
+                    animation: `skillAnim 1s ease .8s 1 normal forwards`
+                })
+                $('#node').css({
+                    animation: `skillAnim 1s ease .9s 1 normal forwards`
+                })
+                $('#vsc').css({
+                    animation: `skillAnim 1s ease 1s 1 normal forwards`
+                })
+                $('#git').css({
+                    animation: `skillAnim 1s ease 1.1s 1 normal forwards`
                 })
             }
         }
     }, []);
 
-    useEffect(() => {
-        window.addEventListener("scroll", onScroll);
-        let index = 0
-        function onScroll() {
-            const scrollTop = $(window).scrollTop();
-            const Offset = document.getElementById('socialLinksTrigger').offsetTop;
-            const vh = (window.innerHeight);
 
-            if (scrollTop < 200 && index > 1) {
-                $('#socialLinks').css({
-                    opacity: 0,
-                    Animation: `socialLinksAnimOut .5s ease 0s 1 reverse backwards`
-                })
-            } else if ((scrollTop + (vh)) > Offset) {
-                $('#socialLinks').css({
-                    opacity: 1,
-                    Animation: `socialLinksAnim .5s ease 0s 1 normal forwards`
-                })
-                index++
-            }
-        }
-    }, []);
+
 
     return (
-        <div id="introArea">
-            <div className="introLeft">
-                <h1>Skills</h1>
-                <h2 id="introText">I'm a <span>graphic designer</span> and <span>frontend developer</span> living in Devon and I'm passionate about creating things that are beautiful both inside and out.</h2>
-                <div id="socialLinksTrigger"></div>
-                <p id="introText">As a graphic designer I have 13+ years of experience working with Adobe Creative suite - primarily <span>Photoshop</span>, <span>Illustrator</span> and <span>Indesign</span> all of which I use on a daily basis. As for web development I have a certification in frontend development using <span>Node.js</span>, <span>React</span>, <span>Bootstrap</span>, <span>Javascipt</span>, <span>CSS</span> and <span>HTML</span>.<br /><br />I look at any project or opportunity to work in a design environment as a chance to learn and develop my skillset.</p>
+        <div id="skillsArea">
+            <div id="skillsRow">
+                <div id="photoshop" className="skill">
+                    <img className="skillIcon" src={photoshop} alt="photoshop" />
+                    <p>Photoshop</p>
+                </div>
+                <div id="illustrator" className="skill">
+                    <img className="skillIcon" src={illustrator} alt="illustrator" />
+                    <p>Illustrator</p>
+                </div>
+                <div id="indesign" className="skill">
+                    <img className="skillIcon" src={indesign} alt="indesign" />
+                    <p>Indesign</p>
+                </div>
+                <div id="HTML" className="skill">
+                    <img className="skillIcon" src={HTML} alt="HTML" />
+                    <p>HTML</p>
+                </div>
+                <div id="CSS" className="skill">
+                    <img className="skillIcon" src={CSS} alt="CSS" />
+                    <p>CSS</p>
+                </div>
+                <div id="js" className="skill">
+                    <img className="skillIcon" src={js} alt="js" />
+                    <p>Javascript</p>
+                </div>
+                <div id="jquery" className="skill">
+                    <img className="skillIcon" src={jquery} alt="jquery" />
+                    <p>JQuery</p>
+                </div>
+                <div id="react" className="skill">
+                    <img className="skillIcon" src={react} alt="react" />
+                    <p>React</p>
+                </div>
+                <div id="bootstrap" className="skill">
+                    <img className="skillIcon" src={bootstrap} alt="bootstrap" />
+                    <p>Bootstrap</p>
+                </div>
+                <div id="node" className="skill">
+                    <img className="skillIcon" src={node} alt="node" />
+                    <p>NodeJS</p>
+                </div>
+                <div id="vsc" className="skill">
+                    <img className="skillIcon" src={vsc} alt="vsc" />
+                    <p>Visual Studio</p>
+                </div>
+                <div id="git" className="skill">
+                    <img className="skillIcon" src={git} alt="git" />
+                    <p>Git</p>
+                </div>
             </div>
         </div>
     );
