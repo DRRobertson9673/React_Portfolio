03f3bd4715c317635b6da1565db6d8a620bb3869
DRRobertson9673 <drrobertson9673@gmail.com>
2023-05-29 16:47:45 +0200

    added new project

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -153,13 +153,25 @@ function Projects() {
                 <h2 id="frontendPortfolioSelector" className="selector currentPortfolio" onClick={showFrontendDevelopment}>Frontend Development</h2>
                 <h2 id="graphicdesignPortfolioSelector" className="selector" onClick={showGraphicDesign}>Graphic Design</h2></div>
             <div id="projectsGrid">
+            <div id="project4" className="project">
+                    <div className="projectOverlay">
+                        <div className="projectInfo">
+                            <h3 className="projectName">KILL TEAM<br />DASHBOARD</h3>
+                            <p className="projectDescription">A personal project of a dashboard for the game 'Kill Team'. Built for a tablet.</p>
+                            <p className="builtWith"><b>Built with: </b>React, Node, Javascript, jQuery, CSS, Netlify</p>
+                            <a href="https://github.com/DRRobertson9673/Tau-Cards" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE CODE <div class="arrow"></div></p></a>
+                            <a href="https://tau-datacards.netlify.app/" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE APP <div class="arrow"></div></p></a>
+                        </div>
+                    </div>
+                </div>
                 <div id="project1" className="project">
                     <div className="projectOverlay">
                         <div className="projectInfo">
                             <h3 className="projectName">WEIGHTLESS<br />WEIGHTLOSS</h3>
-                            <p className="projectDescription"><b>Built with: </b>React, Node, Javascript, CSS, Netlify</p>
-                            <a href="https://github.com/Boxie999/weightless-weightloss" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE CODE <span className="linkArrow">&#9654;</span></p></a>
-                            <a href="https://weightless-weightloss.netlify.app/" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE APP <span className="linkArrow">&#9654;</span></p></a>
+                            <p className="projectDescription">A fitness app where users can pull from a large API of simple excercises to make a workout plan.</p>
+                            <p className="builtWith"><b>Built with: </b>React, Node, Javascript, jQuery, CSS, Netlify</p>
+                            <a href="https://github.com/Boxie999/weightless-weightloss" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE CODE <div class="arrow"></div></p></a>
+                            <a href="https://weightless-weightloss.netlify.app/" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE APP <div class="arrow"></div></p></a>
                         </div>
                     </div>
                 </div>
@@ -167,9 +179,10 @@ function Projects() {
                     <div className="projectOverlay">
                         <div className="projectInfo">
                             <h3 className="projectName">PERFECT<br />MOVIE NIGHT</h3>
-                            <p className="projectDescription"><b>Built with: </b>HTML, Javascript, CSS</p>
-                            <a href="https://github.com/DRRobertson9673/Perfect_Movie_Night" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE CODE <span className="linkArrow">&#9654;</span></p></a>
-                            <a href="https://drrobertson9673.github.io/Perfect_Movie_Night/" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE APP <span className="linkArrow">&#9654;</span></p></a>
+                            <p className="projectDescription">The application utalizes an API to retrieve movie/TV show options from numerous streaming platforms and displays them in one place.</p>
+                            <p className="builtWith"><b>Built with: </b>HTML, Javascript, CSS</p>
+                            <a href="https://github.com/DRRobertson9673/Perfect_Movie_Night" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE CODE <div class="arrow"></div></p></a>
+                            <a href="https://drrobertson9673.github.io/Perfect_Movie_Night/" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE APP <div class="arrow"></div></p></a>
                         </div>
                     </div>
                 </div>
@@ -177,9 +190,10 @@ function Projects() {
                     <div className="projectOverlay">
                         <div className="projectInfo">
                             <h3 className="projectName">WEATHER<br />DASHBOARD</h3>
-                            <p className="projectDescription"><b>Built with: </b>HTML, Javascript, CSS</p>
-                            <a href="https://github.com/DRRobertson9673/Weather_Dashboard" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE CODE <span className="linkArrow">&#9654;</span></p></a>
-                            <a href="https://drrobertson9673.github.io/Weather_Dashboard/" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE APP <span className="linkArrow">&#9654;</span></p></a>
+                            <p className="projectDescription">A simple weather app that allows the user to search for weather forecasts</p>
+                            <p className="builtWith"><b>Built with: </b>HTML, Javascript, CSS</p>
+                            <a href="https://github.com/DRRobertson9673/Weather_Dashboard" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE CODE <div class="arrow"></div></p></a>
+                            <a href="https://drrobertson9673.github.io/Weather_Dashboard/" target="_blank" rel="noopener noreferrer" className="projectLink"><p>VIEW THE APP <div class="arrow"></div></p></a>
                         </div>
                     </div>
                 </div>
